3ce1af03af90ab46b9da3a154e33ed01b7fb8ff0
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-12-05 12:57:33 -0800

    removed unused component

--- src/App.js
+++ src/App.js
@@ -169,7 +169,7 @@ function MealChoice({mealOptionsData, allOptionsData, mealChoice, back}) {
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="grey" className="bi bi-house-door-fill" viewBox="0 0 16 16">
           <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
         </svg>
-        <p id="main-menu-label">Back to Main Menu</p>
+        <p>Back to Main Menu</p>
       </div>
       <div className="meal-picture">
         <img src={mealArray[currentIndex].picture} alt={mealArray[currentIndex].mealName}></img>
@@ -256,13 +256,6 @@ function Recipe({selectedMeal, back}) {
       <div className="back">
           <button onClick={back}>Back to Main Menu</button>
       </div>
-    </div>
-  )
-}
-
-function AddRecipe () {
-  return (
-    <div className="add-recipe">
     </div>
   )
 }
