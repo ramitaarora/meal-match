18c030c0582c470ec472ce17ab3f0d9d9601a25c
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-12-07 10:26:02 -0800

    greyed out links for prev and next step

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,8 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Recipe({selectedMeal, back}) {
   const [step, setStep] = useState(1);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [prevColor, setPrevColor] = useState({color: 'grey', cursor: 'default'})
+  const [nextColor, setNextColor] = useState({color: 'black', cursor: 'pointer'})
+
+  useEffect(() => {
+    if (currentIndex === 0) {
+      setPrevColor({color: 'grey'})
+      setNextColor({color: 'black', cursor: 'pointer'})
+    }
+    if (currentIndex > 0 && currentIndex < selectedMeal.instructions.length - 1) {
+      setPrevColor({color: 'black', cursor: 'pointer'})
+    }
+    if (currentIndex === selectedMeal.instructions.length - 1) {
+      setNextColor({color: 'grey', cursor: 'default'})
+    }
+  }, [currentIndex])
 
   const handleNext = () => {
     if (currentIndex < selectedMeal.instructions.length - 1) {
@@ -47,11 +62,11 @@ export default function Recipe({selectedMeal, back}) {
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="grey" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
           </svg>
-          <p>Previous Step</p>
+          <p style={prevColor}>Previous Step</p>
         </div>
 
         <div id="next-step" onClick={handleNext}>
-          <p>Next Step</p>
+          <p style={nextColor}>Next Step</p>
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="grey" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
           </svg>
